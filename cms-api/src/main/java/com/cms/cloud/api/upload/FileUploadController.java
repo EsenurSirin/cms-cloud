4c97ccab44d811c97417a1d67f57a708213d94c6
Eşenur Şirin <90318335+EsenurSirin@users.noreply.github.com>
2022-09-01 14:22:40 +0300

    FileUpload has been created.

---
+++ cms-api/src/main/java/com/cms/cloud/api/upload/FileUploadController.java
@@ -0,0 +1,33 @@
+package com.cms.cloud.api.upload;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+public class FileUploadController {
+
+    @PostMapping("/uploadFile")
+    public ResponseEntity<FileUpload> uploadFile(
+            @RequestParam("file") MultipartFile multipartFile)
+            throws IOException {
+
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        long size = multipartFile.getSize();
+
+        String filecode = FileUploadUtil.saveFile(fileName, multipartFile);
+
+        FileUpload response = new FileUpload();
+        response.setFileName(fileName);
+        response.setSize(size);
+        response.setDownloadUrl("/downloadFile/" + filecode);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
