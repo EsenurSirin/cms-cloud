ca0f65e0e7d2dbfa4498b5a790310977ab0cd811
Eşenur Şirin <90318335+EsenurSirin@users.noreply.github.com>
2022-10-25 01:20:27 +0300

    Spring boot cron job every 5 minutes

--- cms-api/src/main/java/com/cms/cloud/api/job/CmsCloudJobs.java
+++ cms-api/src/main/java/com/cms/cloud/api/job/CmsCloudJobs.java
@@ -1,13 +1,15 @@
 package com.cms.cloud.api.job;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+@Component
 public class CmsCloudJobs {
 
-    @Scheduled(cron = "0 */5 * ? * *")
+    @Scheduled(cron = "0 */1 * ? * *")
     public void runEvey5Minutes() {
-        System.out.println("Current time is :: " + LocalDate.now());
+        System.out.println("Current time is :: " + LocalDateTime.now());
     }
 }
