f1d698fe0f62b1a9c2f9a4054aee34c94a2b9b02
Eşenur Şirin <90318335+EsenurSirin@users.noreply.github.com>
2022-10-25 02:14:12 +0300

    "HttpUtil.getClientIpAddressIfServletRequestExist()" is defined in "logAddDto.setupIp".

--- cms-api/src/main/java/com/cms/cloud/api/aop/LogAspect.java
+++ cms-api/src/main/java/com/cms/cloud/api/aop/LogAspect.java
@@ -11,10 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @Aspect
