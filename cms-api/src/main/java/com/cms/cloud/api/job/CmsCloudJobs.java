e879fab75f696a65021841af630d47c8a2bf876b
Eşenur Şirin <90318335+EsenurSirin@users.noreply.github.com>
2022-10-26 17:13:45 +0300

    Created "CmsCloudJobs" and "JavaMailConfiguration". Email sent with Spring Boot and Gmail.

--- cms-api/src/main/java/com/cms/cloud/api/job/CmsCloudJobs.java
+++ cms-api/src/main/java/com/cms/cloud/api/job/CmsCloudJobs.java
@@ -1,15 +1,26 @@
 package com.cms.cloud.api.job;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDateTime;
 
 @Component
 public class CmsCloudJobs {
+    @Autowired
+    private JavaMailSender emailSender;
 
     @Scheduled(cron = "0 */1 * ? * *")
-    public void runEvey5Minutes() {
-        System.out.println("Current time is :: " + LocalDateTime.now());
+    public void sendEmailEvery5Minutes() {
+        //email kodu başlangıç
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("ese.siri@gmail.com");
+        message.setTo("ese.siri@gmail.com");
+        message.setSubject("test emaili");
+        message.setText("test içerik");
+        emailSender.send(message);
+        //email kodu bitiş
+        System.out.println("Email Sent Time : " + LocalDateTime.now());
     }
 }
---
+++ cms-api/src/main/java/com/cms/cloud/api/job/JavaMailConfiguration.java
@@ -0,0 +1,28 @@
+package com.cms.cloud.api.job;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class JavaMailConfiguration {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("ese.siri@gmail.com");
+        mailSender.setPassword("hgatoxuxparvqjah");
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        return mailSender;
+    }
+}
+
