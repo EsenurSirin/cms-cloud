2703105c104d4c66afcc2109c521c7ba995835d8
Eşenur Şirin <90318335+EsenurSirin@users.noreply.github.com>
2022-10-27 11:30:15 +0300

    The package "Java Mail Configuration" has been created.

--- cms-api/src/main/java/com/cms/cloud/api/config/JavaMailConfiguration.java
+++ cms-api/src/main/java/com/cms/cloud/api/config/JavaMailConfiguration.java
@@ -1,4 +1,4 @@
-package com.cms.cloud.api.job;
+package com.cms.cloud.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
