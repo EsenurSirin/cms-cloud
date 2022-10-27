14c2c6609bd6bcac0d27832966091990c293810f
Eşenur Şirin <90318335+EsenurSirin@users.noreply.github.com>
2022-10-27 11:34:26 +0300

    The class name has been edited.

--- cms-api/src/main/java/com/cms/cloud/api/job/CmsCloudJob.java
+++ cms-api/src/main/java/com/cms/cloud/api/job/CmsCloudJob.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 
 @Component
-public class CmsCloudJobs {
+public class CmsCloudJob {
     @Autowired
     private JavaMailSender emailSender;
 
